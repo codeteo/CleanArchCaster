c6b926a4681b655dc192ffb2d9d9acfc4ad92602
codeteo <ramones.teo@gmail.com>
2018-06-30 01:30:44 +0300

    Adds Repository Interface for Data layer

---
+++ Data/src/main/java/com/example/data/repository/ProjectsRemote.kt
@@ -0,0 +1,10 @@
+package com.example.data.repository
+
+import com.example.data.model.ProjectEntity
+import io.reactivex.Observable
+
+interface ProjectsRemote {
+
+    fun getProjects(): Observable<List<ProjectEntity>>
+
+}
