3136ddb4027bdfb601663761268ad1bdc012c25e
codeteo <ramones.teo@gmail.com>
2018-06-30 00:39:08 +0300

    Setups Interactors

---
+++ Domain/src/main/java/com/example/domain/interactor/ObservableUseCase.kt
@@ -0,0 +1,32 @@
+package com.example.domain.interactor
+
+import com.example.domain.executor.PostExecutionThread
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class ObservableUseCase<T, in Params> constructor(
+        private val postExecutionThread: PostExecutionThread) {
+
+    private val disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCaseSingle(params: Params? = null): Observable<T>
+
+    open fun execute(observer: DisposableObserver<T>, params: Params? = null) {
+        val observable = this.buildUseCaseSingle(params)
+                .subscribeOn(Schedulers.io())
+                .observeOn(postExecutionThread.scheduler)
+        addDisposable(observable.subscribeWith(observer))
+    }
+
+    fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) disposables.dispose()
+    }
+
+}
